b5dcae70197238eed8296fc851fc802f34c065fe
kimocoder <christian@aircrack-ng.org>
2020-10-16 03:40:33 +0200

    Enable the channel 14 and 80 MHz width channels in regdom

--- os_dep/linux/wifi_regd.c
+++ os_dep/linux/wifi_regd.c
@@ -40,11 +40,11 @@
 
 /* 5G chan 36 - chan 165 */
 #define RTW_5GHZ_5150_5850	\
-	REG_RULE(5150-10, 5850+10, 40, 0, 30,	\
+	REG_RULE(5150-10, 5850+10, 80, 0, 30,	\
 		 NL80211_RRF_PASSIVE_SCAN | NL80211_RRF_NO_IBSS)
 
 static const struct ieee80211_regdomain rtw_regdom_rd = {
-	.n_reg_rules = 3,
+	.n_reg_rules = 4,
 	.alpha2 = "99",
 	.reg_rules = {
 		RTW_2GHZ_CH01_11,
@@ -69,20 +69,20 @@ static struct country_code_to_enum_rd allCountries[] = {
 
 /* 5G chan 36 - chan 64 */
 #define RTW_5GHZ_5150_5350	\
-	REG_RULE(5150-10, 5350+10, 40, 0, 30,	\
+	REG_RULE(5150-10, 5350+10, 80, 0, 30,	\
 		 NL80211_RRF_PASSIVE_SCAN | NL80211_RRF_NO_IBSS)
 
 /* 5G chan 100 - chan 165 */
 #define RTW_5GHZ_5470_5850	\
-	REG_RULE(5470-10, 5850+10, 40, 0, 30, \
+	REG_RULE(5470-10, 5850+10, 80, 0, 30, \
 		 NL80211_RRF_PASSIVE_SCAN | NL80211_RRF_NO_IBSS)
 
 /* 5G chan 149 - chan 165 */
 #define RTW_5GHZ_5725_5850	\
-	REG_RULE(5725-10, 5850+10, 40, 0, 30, \
+	REG_RULE(5725-10, 5850+10, 80, 0, 30, \
 		 NL80211_RRF_PASSIVE_SCAN | NL80211_RRF_NO_IBSS)
 
-
+#if 0
 static const struct ieee80211_regdomain rtw_regdom_11 = {
 	.n_reg_rules = 1,
 	.alpha2 = "99",
@@ -140,7 +140,7 @@ static const struct ieee80211_regdomain rtw_regdom_14 = {
 		RTW_2GHZ_CH14,
 	}
 };
-
+#endif
 
 static struct rtw_regulatory *rtw_regd;
 #endif
@@ -256,17 +256,17 @@ static void _rtw_reg_apply_active_scan_flags(struct wiphy *wiphy,
 
 void rtw_regd_apply_flags(struct wiphy *wiphy)
 {
-#ifndef CONFIG_DISABLE_REGD_C				// by channel plan
+#ifndef CONFIG_DISABLE_REGD_C
 	struct dvobj_priv *dvobj = wiphy_to_dvobj(wiphy);
-	struct rf_ctl_t *rfctl = dvobj_to_rfctl(dvobj);
-	RT_CHANNEL_INFO *channel_set = rfctl->channel_set;
-	u8 max_chan_nums = rfctl->max_chan_nums;
+	//struct rf_ctl_t *rfctl = dvobj_to_rfctl(dvobj);
+	//RT_CHANNEL_INFO *channel_set = rfctl->channel_set;
+	//u8 max_chan_nums = rfctl->max_chan_nums;
 
 	struct ieee80211_supported_band *sband;
 	struct ieee80211_channel *ch;
 	unsigned int i, j;
 	u16 channel;
-	u32 freq;
+	//u32 freq;
 
 	/* all channels enable */
 	for (i = 0; i < NUM_NL80211_BANDS; i++) {
